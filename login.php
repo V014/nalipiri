1ebbf657535c71005cc1a4492852957ded98abab
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-03-26 21:39:14 +0200

    adding new landing page

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" >
+<body>
+<head>
+    <title>Nalipiri Eco Resort - Login</title>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form action="php/login.php" method="POST">
+        <h3>Login Here</h3>
+
+        <label for="username">Username</label>
+        <input type="text" placeholder="Email or Phone" name="username">
+
+        <label for="password">Password</label>
+        <input type="password" placeholder="Password" name="password">
+
+        <button type="submit" name="submit">Login</button>
+    
+        <!-- <div class="social">
+          <div class="go"><i class="fab fa-google"></i>  Google</div>
+          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div> -->
+    </form>
+</body>
+</html>
