b78b0c5d0260bb98ab5d58de9732f14d5e479dfc
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-03-28 16:35:32 +0200

    updated table css to remove css affecting body

--- user_dashboard.php
+++ user_dashboard.php
@@ -37,6 +37,7 @@ if (isset($_SESSION['customer_id']) && !empty($_SESSION['customer_id'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
     <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/table.css">
 
 </head>
 <body>
