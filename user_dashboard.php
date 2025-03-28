48af0ea6aaf497efe4221dcb7d9103b32312b15b
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-03-28 16:48:37 +0200

    updated usage table to show the day of the transaction

--- user_dashboard.php
+++ user_dashboard.php
@@ -414,7 +414,14 @@ if (isset($_SESSION['customer_id']) && !empty($_SESSION['customer_id'])) {
                       <td><?php echo 'MK' . $bill['kWh_usage'] * 150; ?></td>
                       <td><?php echo 'MK' . $bill['water_usage'] * 100 + $bill['kWh_usage'] * 150; ?></td>
                       <td><?php echo $bill['date']; ?></td>
-					  <td><?php echo $bill['day']; ?></td>
+					  <td><?php
+					  			$transaction_datetime = $bill['date'];
+								//Convert the datetime to a DateTime object
+								$date = new DateTime($transaction_datetime);
+								$day_of_week = $date->format('l');
+					  			echo $day_of_week; 
+							?>
+						</td>
                   </tr>
               <?php } ?>
           </tbody>
