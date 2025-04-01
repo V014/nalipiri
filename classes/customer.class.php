69cf058927a4816fcb6d0f39f59f70ec7532dd32
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-01 17:43:51 +0200

    added controller to receive data

--- classes/customer.class.php
+++ classes/customer.class.php
@@ -1,12 +1,18 @@
 <?php
-class Customer extends Dbh {
+class Customer extends Dbh { // gives access to the database through the Dbh class defined in the database file
 
-    protected function getCustomer($user_id){ // protect all user data
+    protected function getCustomer($user_id){ // protect all user data, using session id to find the user in the db
         $sql = "SELECT * FROM customer WHERE id = ?"; // create a query with a temporary query condition
         $stmt = $this->connect()->prepare($sql); // prepare the statement to run sql before adding user data to prevent breach
         $stmt->execute([$user_id]); // execute the statement
 
         $results = $stmt->fetch(); // FetchAll grabs all the data instead of one row
         return $results; // return the results to pass on to the view
+    }
+
+    protected function setCustomer($username, $password, $people){ // specify the type of data you want to manipulate
+        $sql = "INSERT INTO customer(username, password, people) VALUES (?, ?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$username, $password, $people]);
     }
 }
--- classes/customerview.class.php
+++ classes/customerview.class.php
@@ -1,5 +1,14 @@
 <?php
 class CustomerView extends Customer {
 
+    public function showCustomer($user_id) { // set to public to display to user
+        $results = $this->getCustomer($user_id); // collect data from the customer class
+        return $results[0]['username']; // return the data to the user
+    }
+
+    public function createCustomer($username, $password, $people){ // add parameters to allow the types of data to manipulate
+        $this->setCustomer($username, $password, $people); // push the data to the customer class
+    }
+
 }
 ?>
