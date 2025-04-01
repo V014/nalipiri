f8ba2e13f3acb46bfe6a86d738aa0e09b28fc532
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-01 17:52:18 +0200

    moved login and log out files to includes folder

--- classes/customerview.class.php
+++ classes/customerview.class.php
@@ -5,10 +5,5 @@ class CustomerView extends Customer {
         $results = $this->getCustomer($user_id); // collect data from the customer class
         return $results[0]['username']; // return the data to the user
     }
-
-    public function createCustomer($username, $password, $people){ // add parameters to allow the types of data to manipulate
-        $this->setCustomer($username, $password, $people); // push the data to the customer class
-    }
-
 }
 ?>
