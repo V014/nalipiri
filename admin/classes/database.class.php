70e7525a801be796de764d065d0040013c25a0b3
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-07 16:28:45 +0200

    updated the connection file

--- admin/classes/database.class.php
+++ admin/classes/database.class.php
@@ -5,14 +5,74 @@ class Dbh {
     private $password = "";
     private $database = "nalipiri_eco_resort_db";
 
-    protected function connect() {
+    /*
+    Check if the database exists
+    */
+    protected function isSetUp()
+    {
+        $dbh = new PDO("mysql:host=" . $this->host, $this->username, $this->password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $query = $dbh->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '" . DATABASE . "'");
+
+        if (!$query->fetch()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function initDB() {
         try {
-            $dsn = 'mysqli:host=' . $this->host . ';database=' . $this->database;
-            $pdo = new PDO($dsn, $this->username, $this->password); // Creating a new PDO instance
-            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // Setting PDO Fetch method
-            return $pdo;
-        } catch (PDOException $e) {
-            echo "Connection failed: " . $e->getMessage(); // Display error message if the connection fails
+            $dbh = new PDO("mysql:host=" . $this->host, $this->username, $this->password);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $dbQuery = $dbh->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '" . DATABASE . "'");
+            // Create the nalipiri_eco_resort database if it does not exist
+            if (!$dbQuery->fetch()) {
+                $dbh->beginTransaction();
+                $dbh->query("CREATE DATABASE IF NOT EXISTS " . DATABASE);
+                $dbh->query("USE " . DATABASE);
+                // Read contents of nalipiri_eco_resort.sql
+                $sql = file_get_contents(__DIR__ . "/../db/nalipiri_eco_resort.sql") or die("Failed to open nalipiri_eco_resort.sql file");
+                // Execute nalipiri_eco_resort.sql content
+                $dbh->exec($sql);
+            }
+    
+            // Create admin entry
+            $connection = getConnection();
+            $username = "admin";
+            $password = password_hash("secret1234", PASSWORD_DEFAULT);
+    
+            $sql = "INSERT INTO admin (username, password) VALUES (:username, :password)";
+            $sth = $connection->prepare($sql);
+            $sth->execute([
+                ":username" => $username,
+                ":password" => $password
+            ]);
+        } catch (PDOException $exc) {
+            die("Database connection failed: " . $exc->getMessage());
+        }
+    }
+        /*
+    Create PDO instance and retrieve it in subsequent calls without reinitilization
+    */
+    function getConnection()
+    {
+        static $dbh = null;
+
+        try {
+            if (!isSetUp()) {
+                initDB();
+            }
+
+            if ($dbh === null) {
+                $dbh = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
+                $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            }
+
+            return $dbh;
+        } catch (PDOException $exc) {
+            die("Database connection failed " . $exc->getMessage());
         }
     }
 }
